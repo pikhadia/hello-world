f8c76a2a458e091b751e2cd623ab0437a32406e6
pikhadia <47416294+pikhadia@users.noreply.github.com>
2020-11-25 10:53:36 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <HelloWorld msg="Home" />
+    <Card />
+    <!-- <Drawer/> -->
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+// import Drawer from "./components/Drawer.vue";
+import Card from "./components/Card.vue";
+
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    Card,
+    // Drawer,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#app {
+  background-image: url('./assets/cold-bg3.jpg');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,73 @@
+
+<template>
+  <div class="card">
+    <md-card>
+      <md-card-header>
+        <md-card-header-text>
+          <div class="md-title">Weather card</div>
+          <div class="md-subhead">Locate Globally</div>
+        </md-card-header-text>
+          <div class="search-box">
+        <input 
+          type="text" 
+          class="search-bar" 
+          placeholder="Search Places..."
+          v-model="query"
+          @keypress="fetchWeather"
+        />
+      </div>
+        <md-card-media md-medium>
+          <!-- <img src="/assets/logo.png" alt="People"> -->
+        </md-card-media>
+      </md-card-header>
+
+      <md-card-actions>
+        <md-button>Submit Location</md-button>
+      </md-card-actions>
+    </md-card> 
+  </div>
+</template>
+
+<style scoped>
+  .md-card {
+    width: 600px;
+    margin: 4px;
+    display: inline-block;
+    vertical-align: top;
+    background-color: blue;
+  }
+   .card-header {
+   text-align: center;
+   padding: 5px 0px;
+   background: #555;
+   color: #f2f2f2;
+
+ }
+ .search-box .search-bar {
+  display: block;
+  width: 60%;
+  padding: 5px;
+  color: #313131;
+  font-size: 20px;
+  background: none;
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+ }
+ .weather-box {
+  text-align: left;
+}
+ 
+ @media screen and (max-width: 1000px) {
+   .card {
+     width: 75%;
+   }
+ }
+
+ @media screen and (max-width: 620px) {
+   .card {
+     width: 80%;
+   }
+ }
+</style>
+
